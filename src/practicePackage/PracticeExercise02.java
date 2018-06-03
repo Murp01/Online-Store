c9b9bdae782fbaf3cd0bf6efcb9a6e0e1122846c
Murp01 <pmurphy35@hotmail.com>
2018-06-03 21:27:08 +0100

    Practice exercise 02
    Completed

--- src/practicePackage/PracticeExercise02.java
+++ src/practicePackage/PracticeExercise02.java
@@ -1,9 +1,25 @@
 package practicePackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class PracticeExercise02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//01. Launch a new Firefox browser
+		WebDriver driver = new FirefoxDriver();
+		
+		//02. Open http://demoqa.com/frames-and-windows/
+		driver.get("http://demoqa.com/frames-and-windows/");
+		
+		//03. Use this statement to click on a New Window button 
+		driver.findElement(By.xpath(".//*[@id='tabs-1']/div/p/a")).click();
+		
+		//04. close browser
+		driver.close();
+		
+		//note: only one tab will close.  driver.quit will close all
 
 	}
 
