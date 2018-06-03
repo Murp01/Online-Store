408590ea829e573b79745c5dbb8b6cee8e2927dc
Murp01 <pmurphy35@hotmail.com>
2018-06-03 19:51:29 +0100

    Added practice package

---
+++ src/practicePackage/FirstTestCase.java
@@ -0,0 +1,19 @@
+package practicePackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.co.uk/");
+		String pageTitle = driver.getTitle();
+		
+		System.out.println(pageTitle);
+		
+		driver.quit();
+
+	}
+
+}
--- src/webDriverTests/IETest.java
+++ src/webDriverTests/IETest.java
@@ -1,23 +1,15 @@
 package webDriverTests;
 
-import java.io.File;
-
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.ie.InternetExplorerDriverLogLevel;
-import org.openqa.selenium.ie.InternetExplorerDriverService;
 
 public class IETest {
 
 	public static void main(String[] args) {
-		String exePath = "C:\\Users\\abc\\Documents\\IEDriverServer\\IEDriverServer.exe";
-		InternetExplorerDriverService.Builder serviceBuilder = new InternetExplorerDriverService.Builder();
-		serviceBuilder.usingAnyFreePort(); // This specifies that sever can pick any available free port to start
-		serviceBuilder.usingDriverExecutable(new File(exePath)); //Tell it where you server exe is
-		serviceBuilder.withLogLevel(InternetExplorerDriverLogLevel.TRACE); //Specifies the log level of the server
-		serviceBuilder.withLogFile(new File("C:\\Users\\abc\\Documents\\logFile.txt")); //Specify the log file. Change it based on your system
-		InternetExplorerDriverService service = serviceBuilder.build(); //Create a driver service and pass it to Internet explorer driver instance
-		InternetExplorerDriver driver = new InternetExplorerDriver(service);
-		driver.get("http://toolsqa.wpengine.com");
+		 String service = "C:\\Eclipse\\Webdrivers\\IEDriverServer_x64_3.9.0\\IEDriverServer.exe";
+		 System.setProperty("webdriver.ie.driver", service);
+
+		 InternetExplorerDriver  driver = new InternetExplorerDriver();
+		 driver.get("http://demoqa.com");
 
 	}
 
